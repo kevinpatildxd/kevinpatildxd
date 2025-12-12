141a20d301290933722f8c2b73552a7c6a6e2df0
kevinpatildxd <kevinpatil6354@gmail.com>
2025-12-12 20:25:31 +0530

    Remove basePath for username repo

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/kevinpatildxd',
-  assetPrefix: '/kevinpatildxd/',
   images: {
     unoptimized: true,
   },
